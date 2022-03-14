0ecfc96ec2d64b31243190c156f44fcb81b5b879
gsajdok <grzegorzsajdok@outlook.com>
2022-03-14 13:36:37 +0100

    Added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,43 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+
 export const Projects = () => {
+    const Projects = [
+        {
+            id: 1,
+          image: project1,
+          name: "Personal page",
+          description: "My own personal page. You are here!",
+          link: "https://github.com/gsajdok/gsajdok.github.io"
+        },
+        {
+            id: 2,
+            image: project2,
+            name: "Oddam w dobre rece",
+            description: "Page for an item sharing service, using Firebase auth and database",
+            link: "https://github.com/gsajdok/oddam_w_dobre_rece_project"
+        }
+    ];
+
     return (
-        <h2>Projects</h2>
+        <div className="projects_section">
+            <div className="wrapper">
+                <div className="title"><h2>Projects</h2></div>
+                <div className="projects_container">
+                    {Projects.map(element => {
+                        return (
+                            <div className="project" key={element.id}>
+                                <div className="project_image"><img src={element.image} alt=""/></div>
+                                <div className="project_text">
+                                    <div className="project_name">{element.name}</div>
+                                    <div className="project_description">{element.description}</div>
+                                    <div className="project_button"><a href={element.link}>Go to Github</a></div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
     )
 }
