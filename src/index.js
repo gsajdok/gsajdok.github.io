f7bfe075e8bcbeb5ebdf3c8818e39f2e869ec490
gsajdok <grzegorzsajdok@outlook.com>
2022-03-26 20:01:14 +0100

    Removed strict mode

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import {App} from "./components/App";
 import './scss/main.scss'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
