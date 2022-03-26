0d796cc1c14b335c7a05c3587101c6901d58e23b
gsajdok <grzegorzsajdok@outlook.com>
2022-03-26 19:26:13 +0100

    Added responsivness for the header, added navigation to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,13 @@ export const Header = () => {
     return (
         <div className="header_section">
             <h1>gsajdok.github.io</h1>
+            <nav>
+                <ul>
+                    <li>welcome</li>
+                    <li>about me</li>
+                    <li>projects</li>
+                </ul>
+            </nav>
         </div>
     )
 }
